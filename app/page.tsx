227c0fdca85f019058786451b5348ac96b225db1
Jorge <jorestuard@gmail.com>
2023-12-31 17:57:53 +0100

    fix page home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <h1>Hi Patricia!</h1>
+      <Link href={'/about'}>About Page</Link>
+      <h1>Hi Patricia!</h1>
     </main>
   )
 }
